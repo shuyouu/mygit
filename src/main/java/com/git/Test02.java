9c05083d8ed507538298bebf2737d59957ca957e
shuyou <qingshang2020@outlook.com>
2022-11-05 20:48:54 +0800

    修改操作

--- src/main/java/com/git/Test02.java
+++ src/main/java/com/git/Test02.java
@@ -1,7 +1,10 @@
 package com.git;
+import java.io.PrintWriter;
 
 public class Test02 {
     public static void main(String[] args) {
+        System.out.println("test01");
+        System.out.println("test01");
         System.out.println("test01");
     }
 }
